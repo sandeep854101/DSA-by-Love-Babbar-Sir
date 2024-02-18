90c95778a9ac3b7f4d45749ef588ed35c9702b73
sandeep854101 <139211042+sandeep854101@users.noreply.github.com>
2024-02-18 23:51:00 +0530

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "I am Sandeep Kumar" << endl;
+
+
+    // variable test
+
+    int a = 123;
+    cout << a << endl;
+    cout<<sizeof(a)<<endl;
+
+    char b='s';
+    cout<<b<<endl;
+    cout<<sizeof(b)<<endl;
+
+    bool c=true;
+    cout<<c<<endl;
+    cout<<sizeof(c)<<endl;
+
+    float f=10.2;
+    cout<<f<<endl;
+    cout<<sizeof(f)<<endl;
+
+    double d=25555.6;
+    cout<<d<<endl;
+    cout<<sizeof(d)<<endl;
+    
+
+
+
+
+    system("pause"); // for exe file i want to stop exe file
+}
---
+++ 02.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // checking type casting
+    int a='a';
+    cout<<a<<endl;
+
+    char ch=98;
+    cout<<ch<<endl;
+
+    unsigned int s=12;
+    cout<<s<<endl;
+
+    // unsigned int k=-12;
+    // cout<<k<<endl;
+}
